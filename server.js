2ba1ff6f1ceed0576fb0274ec5980c60492f0541
Ryan Kois <ryan.kois@gmail.com>
2016-03-19 23:40:47 -0700

    idk again.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from 'express'
 
 let app = express()
 
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/public/css/style.css', (req, res) => {
+  res.sendFile(__dirname + '/public/css/style.css')
+})
+app.get('/public/dist/bundle.js', (req, res) => {
+  res.sendFile(__dirname + '/public/dist/bundle.js')
 })
 
 app.listen(2000)
